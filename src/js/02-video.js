fbc037a860542d74a2ac54e6f75a0c46be61c556
Sergei Mikhailenko <sersus1991@gmail.com>
2022-09-11 13:26:48 +0300

    2task done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+import Vimeo from '@vimeo/player';
 
+const STORAGE_KEY = 'videoplayer-current-time';
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo(iframe);
+
+player.on('timeupdate', saveTimeUpdateToLS);
+player.setCurrentTime(getTimeFromLS());
+player.on('pause', logPausedTime);
+
+function saveTimeUpdateToLS(data) {
+  localStorage.setItem(STORAGE_KEY, data.seconds);
+}
+
+function getTimeFromLS() {
+  return localStorage.getItem(STORAGE_KEY);
+}
+
+function logPausedTime({ seconds: s }) {
+  s = parseInt(s);
+  const toTime = n => (n >= 10 ? n : '0' + n);
+
+  const time =
+    toTime(Math.floor(s / 3600)) +
+    ':' +
+    toTime(Math.floor((s / 60) % 60)) +
+    ':' +
+    toTime(s % 60);
+
+  console.log('Video paused on: ', time);
+}
